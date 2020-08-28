bf0d08c9cc0e76ff37e4e51cd0754085b93b9bef
yushirui <2514904609@qq.com>
2020-08-28 10:00:56 +0800

    deploy

---
+++ precache-manifest.41874b878c5598d9fe641a708696568f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac96d98d65e22cd4fb3",
+    "url": "/bookkeeping-website/css/app.92dd1fda.css"
+  },
+  {
+    "revision": "15bbd2711faa573de43e5371515f62f7",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "4ac96d98d65e22cd4fb3",
+    "url": "/bookkeeping-website/js/app.2c524dde.js"
+  },
+  {
+    "revision": "6b87133e734c3fa6c9dd",
+    "url": "/bookkeeping-website/js/chunk-vendors.bbed940b.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "8865cb34957afe85293cd59d4e47b7a2",
+    "url": "/bookkeeping-website/qrcode.png"
+  },
+  {
+    "revision": "e20d8795ccac1f6560bb149c6c4aec11",
+    "url": "/bookkeeping-website/qrcode_gitee.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/bookkeeping-website/robots.txt"
+  }
+]);
